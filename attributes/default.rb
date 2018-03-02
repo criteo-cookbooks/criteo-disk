287c3eeb8d4923b9c89a25c4095e0a51ff552593
Baptiste Courtois <b.courtois@criteo.com>
2018-03-02 16:14:02 +0100

    Fix ec2 tests

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,13 +5,13 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 # Example :
-# node['criteo_disk'][DEVICE]['label']
-# node['criteo_disk'][DEVICE]['queue_properties']
-# node['criteo_disk'][DEVICE]['partitions'][NAME]['size']
+# node['criteo_disk'][DEVICE]['label'] = 'gpt'
+# node['criteo_disk'][DEVICE]['queue_properties']['scheduler'] = 'noop'
+# node['criteo_disk'][DEVICE]['partitions'][NAME]['size'] = '11G'
 # node['criteo_disk'][DEVICE]['partitions'][NAME]['id']
-# node['criteo_disk'][DEVICE]['partitions'][NAME]['fstype']
+# node['criteo_disk'][DEVICE]['partitions'][NAME]['fstype'] = 'ext4'
 # node['criteo_disk'][DEVICE]['partitions'][NAME]['mkfs_options']
 # node['criteo_disk'][DEVICE]['partitions'][NAME]['flags']
-# node['criteo_disk'][DEVICE]['partitions'][NAME]['mount_point']
-# node['criteo_disk'][DEVICE]['partitions'][NAME]['mount_options']
+# node['criteo_disk'][DEVICE]['partitions'][NAME]['mount_point'] = '/tmp'
+# node['criteo_disk'][DEVICE]['partitions'][NAME]['mount_options'] = 'defaults'
 default['criteo_disk'] = {}
